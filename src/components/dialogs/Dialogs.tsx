3153b6c83929aa0a7cfc2f28138047510346dba9
Halina2610 <vievamaxa@gmail.com>
2023-09-18 02:41:22 +0300

    21 lesson

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,10 +1,69 @@
 import React from 'react';
 import './../../App.css';
+import classes from './Dialogs.module.css';
+import Message from './massages/message/Message';
+import FriendMessage from './massages/friend-message/FriendMessage';
+import MessageSender from './massages/message-sender/MessageSender';
+import avatar from './avatar.png'
+
+export type MessageType = {
+    id: number
+    user: {
+        avatar: string;
+        name: string
+    },
+    message: {
+        text: string
+        time: string
+    },
+}
+
+// структуру объекта не менять
+export const message0: MessageType = {
+    id: 0,
+    user: {
+        avatar: avatar, // можно менять
+        name: 'Some Name',  // можно менять
+    },
+    message: {
+        text: 'some textsome textsome textsome text', // можно менять
+        time: '9:00', // можно менять
+    },
+}
+export const friendMessage0: MessageType = {
+    id: 100,
+    user: {
+        avatar: avatar, // можно менять
+        name: 'Friend Name', // можно менять
+    },
+    message: {
+        text: 'зеркальное сообщение для тренировки css', // можно менять
+        time: '9:00', // можно менять
+    },
+}
 
 export const Dialogs = () => {
     return (
-        <div>
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>
+                    Murkiss
+                </div>
+                <div className={classes.dialog}>
+                    Miu
+                </div>
+                <div className={classes.dialog}>
+                    Kitty
+                </div>
+            </div>
 
+            <div className={classes.massage}>
+                    <div>
+                        <Message message={message0} />
+                        <FriendMessage message={friendMessage0} />
+                    </div>
+                    <MessageSender M={Message} />
+            </div>
         </div>
     );
 };
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import classes from './Navbar.module.css';
+import {NavLink} from 'react-router-dom';
+
+interface MenuItem {
+    path: string;
+    label: string;
+}
 
 export const Navbar = () => {
+    const menuItems: MenuItem[] = [
+        {path: '/profile', label: 'Profile'},
+        {path: '/dialogs', label: 'Message'},
+        {path: '/news', label: 'News'},
+        {path: '/music', label: 'Music'},
+        {path: '/settings', label: 'Settings'}
+    ];
+
     return (
         <nav className={classes.nav}>
-            <div><a>Profile</a></div>
-            <div><a>Messager</a></div>
-            <div><a>News</a></div>
-            <div><a>Music</a></div>
-            <div><a>Settings</a></div>
+            {menuItems.map((item) => (
+                <div className={classes.item}>
+                    <NavLink to={item.path} activeClassName={classes.activeLinc}>
+                        {item.label}
+                    </NavLink>
+                </div>
+            ))}
         </nav>
     );
 };
-
